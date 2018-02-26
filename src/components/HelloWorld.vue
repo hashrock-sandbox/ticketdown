cbadfe08302dfb28d8d1ecf0778d8e47c60231b3
hashrock <hashrock@users.noreply.github.com>
2018-02-27 02:49:22 +0900

    feat: add gantt, issues

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,9 @@
       ticketdown - prototype
     </div>
     <textarea class="editor" v-model="source"></textarea>
+    <!--
     <textarea class="editor" v-model="output"></textarea>
+    -->
     <div>
       <select v-model="mode">
         <option>calendar</option>
@@ -16,13 +18,20 @@
 
     <div v-if="mode === 'calendar'">
       <h1>予定</h1>
-      <div v-for="(ticket, idx) in tickets" :key="idx">
+      <div class="calendar__item" v-for="(ticket, idx) in tickets" :key="idx">
         {{ticket.start_at}} {{ticket.name}}
       </div>
     </div>
+    <div v-if="mode === 'issue'">
+      <h1>Issue一覧</h1>
+      <div class="calendar__item" v-for="(ticket, idx) in issues" :key="idx">
+        {{ticket.name}} <span v-if="ticket.user">👤{{ticket.user}}</span> <span class="tag" v-for="(tag, ti) in ticket.tags" :key="ti">#{{tag}}</span>
+      </div>
+    </div>
     <div v-if="mode === 'gantt'">
+      <h1>ガントチャート</h1>
       <svg width="300" height="300">
-        <rect x=0 y=0 width=100 height=100></rect>
+        <rect class="gantt__line" v-for="(ticket, idx) in tickets" :x="d2s(ticket.start_at)" :y="idx * 24" :key="idx" :width="r2e(ticket.start_at, ticket.end_at)" height=22></rect>
       </svg>
     </div>
   </div>
@@ -30,7 +39,7 @@
 
 <script lang="ts">
 import Vue from "vue";
-
+import * as scale from "d3-scale";
 interface Ticket {
   name: string;
   start_at: string;
@@ -45,8 +54,11 @@ interface Ticket {
 
 function parseLine(src: string): Ticket {
   const items = src.split(" ");
-  const user = items.find(i => !!i.match(/^👤.*/g));
-  const alarmAt = items.find(i => !!i.match(/^⏰.*/g));
+  const user = items.find((i: string) => !!i.match(/^👤.*/g));
+  const alarmAt = items.find((i: string) => !!i.match(/^⏰.*/g));
+  const tags = items
+    .filter((i: string) => !!i.match(/^#.*/g))
+    .map((i: string) => i.replace(/#/g, ""));
 
   return {
     name: items[0],
@@ -54,7 +66,7 @@ function parseLine(src: string): Ticket {
     end_at: items[2],
     alarm_at: alarmAt ? alarmAt.replace(/⏰/g, "") : "",
     user: user ? user.replace(/👤/g, "") : "",
-    tags: [],
+    tags: tags,
     attributes: {}
   };
 }
@@ -65,8 +77,9 @@ function parseLines(src: string): Ticket[] {
 
 let source = `タスク1 2018-01-01 2018-01-03 👤yamada ⏰2018-01-02
 タスク2 2018-01-02 2018-01-05
-マイルストーン 2018-01-05
-`;
+Issue1 2018-01-02 2018-01-05 #Open 👤yamada
+Issue2 2018-01-03 2018-01-05 #Open
+マイルストーン 2018-01-05`;
 
 export default Vue.extend({
   name: "HelloWorld",
@@ -76,7 +89,12 @@ export default Vue.extend({
   data() {
     return {
       source: source,
-      mode: "calendar"
+      mode: "calendar",
+      svgWidth: 600,
+      timeRange: [
+        new Date(2018, 0, 1).getTime(),
+        new Date(2018, 0, 20).getTime()
+      ]
     };
   },
   computed: {
@@ -85,6 +103,35 @@ export default Vue.extend({
     },
     output(): string {
       return JSON.stringify(parseLines(this.source), null, 2);
+    },
+    issues(): Ticket[] {
+      return this.tickets.filter(i => i.tags.find(t => !!t.match("Open")));
+    }
+  },
+  methods: {
+    d2s(d: string) {
+      return this.scale(new Date(d).getTime());
+    },
+    r2e(d: string, d2: string) {
+      if (!d2) {
+        return 1000 * 60 * 60 * 24;
+      }
+      return (
+        this.scale(new Date(d2).getTime()) - this.scale(new Date(d).getTime())
+      );
+    },
+    scale(epoc: number) {
+      return scale
+        .scaleLinear()
+        .domain(this.timeRange)
+        .range([0, this.svgWidth])(epoc);
+    },
+    invert(x: number) {
+      return scale
+        .scaleLinear()
+        .domain(this.timeRange)
+        .range([0, this.svgWidth])
+        .invert(x);
     }
   }
 });
@@ -102,5 +149,20 @@ body {
   margin: 1rem;
   padding: 1rem;
   background: white;
+}
+.gantt__line {
+  fill: #557;
+}
+.calendar__item {
+  border: 1px solid #557;
+  padding: 0.5rem;
+}
+.tag {
+  background: #66a585;
+  color: white;
+  display: inline-block;
+  padding: 0rem 0.5rem;
+  border-radius: 4px;
+  font-size: 0.75rem;
 }
 </style>
