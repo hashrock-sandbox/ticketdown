8982be43ceecd0e86a67788bad1b0a04637666a6
hashrock <hashrock@users.noreply.github.com>
2018-02-27 22:24:51 +0900

    chore: rename MainPage

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <MainPage msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-  
-  import Vue from "vue";
-  import HelloWorld from "./components/HelloWorld.vue";
-  import "bootstrap/dist/css/bootstrap-reboot.min.css";
-  export default Vue.extend({
-    name: "app",
-    components: {
-      HelloWorld
-    },
-  });
+import Vue from "vue";
+import MainPage from "./components/MainPage.vue";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+export default Vue.extend({
+  name: "app",
+  components: {
+    MainPage
+  }
+});
 </script>
